a77f8f72da4e40907a6f81102cc084ee52ce6074
Matthieu <matthieu.ospici@free.fr>
2023-12-02 18:57:54 +0100

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod day1;
+mod day2;
 
 fn main() {
     let (d1_p1, d1_p2) = day1::day1("/home/matthieu/dev/adventofcode2023/adventofcode2023/input_files/input_day1");
-
+    let (d2_p1, d2_p2) = day2::day2("/home/matthieu/dev/adventofcode2023/adventofcode2023/input_files/input_day2");
     
     println!("d11 {} d12 {}",d1_p1, d1_p2);
+    println!("d21 {} d22 {}",d2_p1, d2_p2);
 }
